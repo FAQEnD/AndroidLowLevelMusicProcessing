cc687cd1bd077364d5f5d6ae7900268242ab2265
Anton Musiienko <anton.s.musiienko@globallogic.com>
2016-04-13 14:10:40 +0300

    Recording of voice was fixed
    -Code was refactored

---
+++ app/src/main/obj/local/x86_64/objs/FrequencyDomain/LowLevelMusicProcessor.o.d
@@ -0,0 +1,44 @@
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/obj/local/x86_64/objs/FrequencyDomain/LowLevelMusicProcessor.o: \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/LowLevelMusicProcessor.cpp \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFrequencyDomain.h \
+  Superpowered/SuperpoweredAudioBuffers.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredSimple.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredRecorder.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/LowLevelMusicProcessor.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredAndroidAudioIO.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredAdvancedAudioPlayer.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredRoll.h \
+  Superpowered/SuperpoweredFX.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFilter.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFlanger.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredEcho.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredReverb.h \
+  /home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Utils.h
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFrequencyDomain.h:
+
+Superpowered/SuperpoweredAudioBuffers.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredSimple.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredRecorder.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/LowLevelMusicProcessor.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredAndroidAudioIO.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredAdvancedAudioPlayer.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredRoll.h:
+
+Superpowered/SuperpoweredFX.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFilter.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredFlanger.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredEcho.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Superpowered/SuperpoweredReverb.h:
+
+/home/antonsmusiienko/StudioProjects/SuperPowerSDKExample/FrequencyDomain/app/src/main/jni/Utils.h:
